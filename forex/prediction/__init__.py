855d7d6f970910a54318aad4896912149a4179f4
Nicolas <nicolas.saez.v@mail.pucv.cl>
2026-06-18 12:48:09 -0400

    Actualizar __init__.py

--- forex/prediction/__init__.py
+++ forex/prediction/__init__.py
@@ -1,1 +1,12 @@
-
+1H Format
+ OHLC
+ Volume
+ Spread
+ RSI_14
+ MACD, MACD_signal, MACD_hist
+ ATR_14
+ EMA20, EMA50, EMA200
+ Bollinger Bands
+ Returns
+ Volatility_24h
+ Session (Tokyo/London/NewYork)
