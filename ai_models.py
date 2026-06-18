62edee3aa3150c585788be4c75eca0b27772bfee
nicolassaek01 <nicolas.saez.v@mail.pucv.cl>
2026-06-18 11:37:09 -0400

    commit

--- ai_models.py
+++ ai_models.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 import psutil
 from memory import cargar_memoria, guardar_memoria
 
-# Librerías de IA/ML que ya tienes instaladass
+# Librerías de IA/ML que ya tienes instaladass wow
 import torch
 import tensorflow as tf
 import keras
