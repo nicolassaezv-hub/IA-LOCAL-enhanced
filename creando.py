6728568268fffcbeb9a8f9044a5c2899fee0159e
Nicolas <nicolas.saez.v@mail.pucv.cl>
2026-06-25 10:49:17 -0400

    Actualizar creando.py

--- creando.py
+++ creando.py
@@ -7,7 +7,8 @@ import os
 # =====================================
 # CONFIG
 # =====================================
-
+#TIMEFRAMES SON AHORA H1(2025-HOY),H4(2023-HOY)
+#Y D1 (2021-HOY
 TIMEFRAME = mt5.TIMEFRAME_H1
 
 START_DATE = datetime.now() - timedelta(days=365)
